56dda7f7301b7ae5a8883daa46f51f36b77c4e59
sunilshiv <sunil.d.shiv@gmail.com>
2025-12-09 15:28:12 +0530

    Add splash screen and related dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -29,6 +29,7 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+            implementation(libs.splash.screen)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
--- composeApp/src/androidMain/kotlin/com/sds/nutrifysports/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/sds/nutrifysports/MainActivity.kt
@@ -6,10 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
+        installSplashScreen()
         super.onCreate(savedInstanceState)
 
         setContent {
